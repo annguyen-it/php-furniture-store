593b6531e0170114fac35c6bba31620fb162307e
annguyen <annguyen.it.utc@gmail.com>
2022-05-10 10:27:42 +0700

    common layout

---
+++ common/footer.php
@@ -0,0 +1,214 @@
+<footer id="footer" class="js-footer">
+  <div class="footer-container footer-h1 style-1">
+    <div class="footer-top">
+      <div class="container">
+        <div class="footer-blockemail">
+          <!-- begin module:ps_emailsubscription/views/templates/hook/ps_emailsubscription.tpl -->
+          <!-- begin /var/www/html/demo/rb_davici_demo/themes/rb_davici/modules/ps_emailsubscription/views/templates/hook/ps_emailsubscription.tpl -->
+          <div class="ps-emailsubscription-block block links accordion_small_screen" id="blockEmailSubscription_displayRbEmail">
+            <div class="title-newsletter">
+              <h3 class="rb-title">
+                Join now and get 10% off your next purchase!
+              </h3>
+              <p class="sub-letter">
+                You may unsubscribe at any moment. For that purpose,
+                please find our contact info in the legal notice.
+              </p>
+            </div>
+            <div class="block_content">
+              <form action="https://rubiktheme.com/demo/rb_davici_demo/en/#blockEmailSubscription_displayRbEmail" method="post">
+                <div class="input-group newsletter-input-group">
+                  <input class="form-control input-subscription" name="email" type="email" value="" placeholder="Your email address" aria-labelledby="block-newsletter-label" required />
+                  <button class="btn btn-outline" name="submitNewsletter" type="submit">
+                    <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                    <span>Subscribe</span>
+                  </button>
+                </div>
+                <input type="hidden" name="blockHookName" value="displayRbEmail" />
+                <input type="hidden" name="action" value="0" />
+              </form>
+              <div class="msg-block"></div>
+            </div>
+          </div>
+          <!-- end /var/www/html/demo/rb_davici_demo/themes/rb_davici/modules/ps_emailsubscription/views/templates/hook/ps_emailsubscription.tpl -->
+          <!-- end module:ps_emailsubscription/views/templates/hook/ps_emailsubscription.tpl -->
+        </div>
+      </div>
+    </div>
+    <!-- end footer-top-->
+
+    <div class="footer-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 col-sp-12">
+            <!-- begin modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+            <div class="links block RbBlockLink">
+              <p class="h3 title_block">Shop</p>
+              <ul>
+                <li>
+                  <a href="#"> Our Story </a>
+                </li>
+                <li>
+                  <a href="#"> Visit Melbourne Studio </a>
+                </li>
+                <li>
+                  <a href="#"> Visit Sydney Studio </a>
+                </li>
+                <li>
+                  <a href="#"> Visit Brisbane Studio </a>
+                </li>
+                <li>
+                  <a href="#"> Design </a>
+                </li>
+                <li>
+                  <a href="#"> How Davici Works </a>
+                </li>
+              </ul>
+            </div>
+
+            <!-- end modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 col-sp-12">
+            <!-- begin modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+            <div class="links block RbBlockLink">
+              <p class="h3 title_block">Help</p>
+              <ul>
+                <li>
+                  <a href="contact-us.html" title="Use our form to contact us">
+                    Contact us
+                  </a>
+                </li>
+                <li>
+                  <a href="content/1-delivery.html" title="Our terms and conditions of delivery">
+                    Delivery
+                  </a>
+                </li>
+                <li>
+                  <a href="login856b.html"> Order history </a>
+                </li>
+                <li>
+                  <a href="#"> Visit Brisbane Studio </a>
+                </li>
+                <li>
+                  <a href="#"> Interest Free Finance </a>
+                </li>
+                <li>
+                  <a href="#"> Cipmoney </a>
+                </li>
+              </ul>
+            </div>
+
+            <!-- end modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 col-sp-12">
+            <!-- begin modules/rbthemefunction/views/templates/hook/rb-popup.tpl -->
+            <div class="sale-popup flex box-center" data-text1="Just purchased" data-text2="minutes ago"></div>
+            <!-- end modules/rbthemefunction/views/templates/hook/rb-popup.tpl -->
+
+            <!-- begin modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+            <div class="links block RbBlockLink">
+              <p class="h3 title_block">Services</p>
+              <ul>
+                <li>
+                  <a href="#"> Assembly Guides </a>
+                </li>
+                <li>
+                  <a href="#"> Furniture Packages &amp; Fitouts </a>
+                </li>
+                <li>
+                  <a href="#"> Trade Programme </a>
+                </li>
+                <li>
+                  <a href="#"> Sale </a>
+                </li>
+                <li>
+                  <a href="#"> New Designs </a>
+                </li>
+                <li>
+                  <a href="#"> Gift Cards </a>
+                </li>
+              </ul>
+            </div>
+
+            <!-- end modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 col-sp-12">
+            <!-- begin /var/www/html/demo/rb_davici_demo/themes/rb_davici/modules/rbthemedream/views/templates/hook/rb-social.tpl -->
+            <div class="rb-social-block block links">
+              <h3 class="rb-title hidden-sm-down">Follow us</h3>
+              <div id="footer_social_block">
+                <a href="#">
+                  <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                  <span>Facebook</span>
+                </a>
+
+                <a href="#">
+                  <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                  <span>Twitter</span>
+                </a>
+
+                <a href="#">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                  <span>Instagram</span>
+                </a>
+
+                <a href="#">
+                  <i class="fa fa-pinterest-square" aria-hidden="true"></i>
+                  <span>Pinterest</span>
+                </a>
+              </div>
+            </div>
+            <!-- end /var/www/html/demo/rb_davici_demo/themes/rb_davici/modules/rbthemedream/views/templates/hook/rb-social.tpl -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end footer-center -->
+
+    <div class="footer-copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12 col-sp-12">
+            <div class="text-md-left">
+              <a class="_blank" href="http://www.prestashop.com/" target="_blank">
+                © 2022 - Ecommerce software by PrestaShop™
+              </a>
+
+              <!-- begin modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+              <div class="links block RbBlockLink">
+                <p class="h3 title_block">Link</p>
+                <ul>
+                  <li>
+                    <a href="#"> Privacy </a>
+                  </li>
+                  <li>
+                    <a href="#"> Terms </a>
+                  </li>
+                  <li>
+                    <a href="#"> *Promo T&amp;Cs Apply (view here) </a>
+                  </li>
+                </ul>
+              </div>
+
+              <!-- end modules/rbthemedream/views/templates/hook/rb_link.tpl -->
+            </div>
+          </div>
+          <div class="col-md-4 col-xs-12 col-sp-12">
+            <div class="text-md-right">
+              <img class="img-fluid" src="./themes/rb_davici/assets/img/payment.png" alt="Davici - Furniture Store" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end footer-copyright -->
+  </div>
+
+  <div id="rb-back-top" class="progress-wrap">
+    <a href="#">
+      <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+      </svg>
+    </a>
+  </div>
+</footer>
