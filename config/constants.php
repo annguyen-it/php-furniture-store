46f3d67945c2ab6d5e573cb3b6fb019448c0232b
annguyen <annguyen.it.utc@gmail.com>
2022-05-11 11:58:49 +0700

    Update code

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!defined('HOST')) define('HOST', 'localhost');
-if (!defined('USER')) define('USER', 'root');
-if (!defined('PASSWORD')) define('PASSWORD', '');
+$ini_array = parse_ini_file('db.ini');
+
+if (!defined('HOST')) define('HOST', $ini_array['HOST'] ?? 'localhost');
+if (!defined('USER')) define('USER', $ini_array['USER'] ?? 'root');
+if (!defined('PASSWORD')) define('PASSWORD', $ini_array['PASSWORD'] ?? '');
 if (!defined('DATABASE_NAME')) define('DATABASE_NAME', 'ecommerceapp');
 
 if (!defined('CURRENCY')) define('CURRENCY', 'Rs');
